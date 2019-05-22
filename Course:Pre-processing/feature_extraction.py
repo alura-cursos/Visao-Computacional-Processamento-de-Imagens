ebce3e433b5e5342031931c9b9e884d18f3559b4
suayder <suayder@uft.edu.br>
2019-05-22 18:00:06 -0300

    ready bag of visual words

--- Course:Pre-processing/feature_extraction.py
+++ Course:Pre-processing/feature_extraction.py
@@ -3,12 +3,12 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import os
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import KMeans
 
 def bovw_gerarDicionario(lista_descritores):
 
-    kmeans = KMeans(n_clusters = 600)
+    kmeans = KMeans(n_clusters = 256)
     kmeans = kmeans.fit(lista_descritores)
     global dicionario
     dicionario = kmeans.cluster_centers_
@@ -29,7 +29,7 @@ def getDescritores(img_caminho):
     img_suavizada = cv2.GaussianBlur(img_redimencionada, (7,7),1)
     img_equalizada = cv2.equalizeHist(img_suavizada)
 
-    orb = cv2.ORB_create(nfeatures = 400)
+    orb = cv2.ORB_create(nfeatures = 512)
 
     # Determinar key points
     pontos_chave = orb.detect(img_equalizada, None)
@@ -39,9 +39,11 @@ def getDescritores(img_caminho):
     return descritores
 
 def bovw_computarDescritores(descritores):
-    algoritmo_knn = KNeighborsClassifier(n_neighbors=1)
-    res = algoritmo_knn.fit(descritores, dicionario)
-    print(res.shape)
+    algoritmo_knn = NearestNeighbors(n_neighbors=1)
+    algoritmo_knn.fit(dicionario)
+    res = algoritmo_knn.kneighbors(descritores, return_distance=False).flatten()
+    histograma_caracteristicas = np.histogram(res, bins=np.arange(dicionario.shape[0]))[0]
+    return histograma_caracteristicas
 
 def salvarDescritores(descritores, caminho):
     descritores = descritores.reshape((1,descritores.size))
@@ -51,7 +53,7 @@ def salvarDescritores(descritores, caminho):
     np.savetxt(arquivo, descritores, delimiter=',', fmt='%i')
     arquivo.close()
 
-def salvarDicionario():
+def bovw_salvarDicionario():
     global dicionario
     if dicionario is not None:
         #print("Shape dicionario antes: ", dicionario.shape)
@@ -61,25 +63,50 @@ def salvarDicionario():
     else:
         print("Dicionario is none")
 
+def bovw_carregarDicionario():
+    global dicionario
+    dicionario = np.loadtxt('dicionario.csv', delimiter=',')
+
+# Tenho que salvar o dicionario
+# gerar as features
+# salvar e então classificar
 def main():
+    global dicionario
     caminhos = ['/home/suayder/Documents/alura/People-Detection-Image-classification/Course:Pre-processing/INRIAPerson_Dataset/Train/positivos/',
     '/home/suayder/Documents/alura/People-Detection-Image-classification/Course:Pre-processing/INRIAPerson_Dataset/Train/negativos/']
+    
+    # Rotina para criação do dicionário de palavras virtuais
     descritores = np.empty((0,32), dtype=np.uint8)
     for caminho in caminhos:
         i = 0
         # r=raiz, d=diretorios, a = arquivos
         for r, d, a in os.walk(caminho):
             for arquivo in a:
-                if i >= 200:
+                if i >= 400: #Somente 400 exemplos positivos e 400 exemplos negativos
                     break
                 if '.png' in arquivo:
                     descritores = np.append(descritores, getDescritores(os.path.join(r, arquivo)), axis=0)
                     i+=1
-    # Bag of Visual words
+
+    ## Bag of Visual words - gerar dicionário de palavras
     bovw_gerarDicionario(descritores)
-    salvarDicionario()
-    #bagOfVisualWords(descritores)
-    #salvarDescritores(descritores, caminho)
+    bovw_salvarDicionario()
+    
+    # Salvar histograma de descritores de exemplos positivos e negativos de cada imagem
+    """ bovw_carregarDicionario() """
+
+    for caminho in caminhos:
+        i = 0
+        # r=raiz, d=diretorios, a = arquivos
+        for r, d, a in os.walk(caminho):
+            for arquivo in a:
+                if i >= 400: #Somente 400 exemplos positivos e 400 exemplos negativos
+                    break
+                if '.png' in arquivo:
+                    descritor = getDescritores(os.path.join(r, arquivo))
+                    hitograma_descritor = bovw_computarDescritores(descritores)
+                    salvarDescritores(hitograma_descritor, caminho)
+                    i+=1  
 
 if __name__ == "__main__":
     main()
